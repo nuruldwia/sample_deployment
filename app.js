d65c26480f8017f83d151c699fdac673f2ff6c1a
nuruldwia <nuruldwia@students.unnes.ac.id>
2023-11-06 20:18:17 +0700

    try sentry.io

--- app.js
+++ app.js
@@ -2,12 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const { PORT = 3000 } = process.env;
+const { PORT, SENTRY_DSN, ENV } = process.env;
+const Sentry = require('@sentry/node');
+
+Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Sentry.Integrations.Express({ app }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    environment: ENV
+})
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.get('/', (req, res) => {
+    console.log(name);
     return res.json({
         status: true,
         message: 'Hello World!',
@@ -18,5 +39,26 @@ app.get('/', (req, res) => {
 
 const authRouter = require('./routes/auth.routes');
 app.use('/api/v1/auth', authRouter);
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        status: false,
+        message: 'Not Found!',
+        error: null,
+        data: null
+    });
+});
+
+app.use((req, res, next) => {
+    return res.status(500).json({
+        status: false,
+        message: 'Internal Message Error!',
+        error: error.message,
+        data: null
+    });
+});
 
 app.listen(PORT, () => console.log('Listening on port', PORT));
